068c1cc68587f59d886769fdd575fde3ec533f65
Scentral <60952407+scentral@users.noreply.github.com>
2024-06-30 07:13:33 -0500

    Update client.lua

--- source/client.lua
+++ source/client.lua
@@ -1,4 +1,5 @@
 -- send ped health to html
+local ped = PlayerPedId()
 
 Citizen.CreateThread(function() 
     while true do
@@ -29,7 +30,6 @@ end)
 Citizen.CreateThread(function() 
     while true do
         Citizen.Wait(2)
-        local ped = PlayerPedId()
         if not IsPedInAnyVehicle(ped, false) then
             SendNUIMessage({
                 type = "radar",
@@ -58,5 +58,53 @@ Citizen.CreateThread(function()
                 show = false
             })
         end
+    end
+end)
+
+Citizen.CreateThread(function()
+    local minimap = RequestScaleformMovie("minimap")
+    SetRadarBigmapEnabled(true, false)
+    Citizen.Wait(0)
+    SetRadarBigmapEnabled(false, false)
+
+    SetMinimapComponentPosition('minimap', 'L', 'B', -0.0050, -0.030, 0.170, 0.200)
+
+    SetMinimapComponentPosition('minimap_mask', 'L', 'B', 0.020, 0.115, 0.125, 0.120)
+  
+    SetMinimapComponentPosition('minimap_blur', 'L', 'B', -0.040, -0.01, 0.306, 0.237)
+    while true do
+        Citizen.Wait(0)
+        BeginScaleformMovieMethod(minimap, "SETUP_HEALTH_ARMOUR")
+        ScaleformMovieMethodAddParamInt(3)
+        EndScaleformMovieMethod()
+        SetBlipAlpha(GetNorthRadarBlip(), 0)
+
+        local waypointBlip = GetFirstBlipInfoId(8)
+        if DoesBlipExist(waypointBlip) then
+            SetBlipAsShortRange(waypointBlip, true)
+        end
+    end
+end)
+
+-- Hide radar when player is not in a vehicle
+Citizen.CreateThread(function() 
+    while true do
+        Citizen.Wait(1)
+        if not IsPedInAnyVehicle(ped, false) then
+            DisplayRadar(false)
+        else
+            DisplayRadar(true)
+        end 
+    end
+end)
+
+Citizen.CreateThread(function()
+    while true do
+		Citizen.Wait(1)
+		if IsPedOnFoot(GetPlayerPed(-1)) then 
+			SetRadarZoom(1100)
+		elseif IsPedInAnyVehicle(GetPlayerPed(-1), true) then
+			SetRadarZoom(1100)
+		end
     end
 end)
