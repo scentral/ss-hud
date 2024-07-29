47010592b5cfa187b86615a2906e11f7372ac9c7
Scentral <60952407+scentral@users.noreply.github.com>
2024-07-29 10:14:31 -0500

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game "gta5"
 lua54 "yes"
 
 author 'Scentral'
-description 'TCRP Framework'
+description 'A simple hud for FiveM'
 version '1.0.0'
 
 client_scripts {
@@ -14,6 +14,5 @@ ui_page "interface/index.html"
 files {
     'interface/index.html',
     'interface/script.js',
-    'interface/style.css',
-    'interface/img/*.png'
+    'interface/style.css'
 }
